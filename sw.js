ae22210e13e8ed1a0269d2149fbf7d1cb6ed682d
Kyle Cordes <kyle.cordes@oasisdigital.com>
2018-01-04 21:47:50 -0600

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts('workbox-sw.prod.v2.1.1.js');
+importScripts('workbox-sw.prod.v2.1.2.js');
 
 /**
  * DO NOT EDIT THE FILE MANIFEST ENTRY
@@ -20,35 +20,51 @@ importScripts('workbox-sw.prod.v2.1.1.js');
 const fileManifest = [
   {
     "url": "build/app.js",
-    "revision": "d371b265b3e42e25fee3d89e7585ee45"
+    "revision": "c797349edc5d0556666ed9cfbcaa54c7"
   },
   {
-    "url": "build/app/5e3mm8py.js",
-    "revision": "d8783a51d88a5fe606e0bd94a40261be"
+    "url": "build/app/app.9reafjny.js",
+    "revision": "4542e3dc54fc51939a0b2608bdc9e186"
   },
   {
     "url": "build/app/app.global.js",
-    "revision": "905896db4c96c2ffe4285cd63c098ca5"
+    "revision": "48bf709aad33b0f1e2b6f3f55316fa1f"
   },
   {
-    "url": "build/app/app.hyykteyc.js",
-    "revision": "491ea638ec574bd546bed4428db40672"
+    "url": "build/app/app.gq0itnbj.js",
+    "revision": "7dd14b7342e449d4f4bff725431db828"
   },
   {
-    "url": "build/app/app.l9sq98xw.js",
-    "revision": "f26e4648447988dc6b18096e02ac197c"
+    "url": "build/app/app.oymxxdod.js",
+    "revision": "3d6d7f7e251802f6a59326ec54a0aada"
   },
   {
     "url": "build/app/app.registry.json",
-    "revision": "7aeb7f28d191e22c44b550f8c64190d4"
+    "revision": "79696a2ac50e493f974d68e15dc97818"
+  },
+  {
+    "url": "build/app/butkeid7.js",
+    "revision": "90e4e7a2576543bd6bded1bc83d78aa4"
   },
   {
     "url": "build/app/cv7cbtbi.js",
     "revision": "b425d7fba0b1c25730cf13a34af6b556"
   },
   {
-    "url": "build/app/n90yxkmc.js",
-    "revision": "4344796cdb8371debe6db0776a69d7f0"
+    "url": "build/app/orsdhirh.js",
+    "revision": "7438a81d2db06c201be7ba516d9071ca"
+  },
+  {
+    "url": "build/app/pbssls7g.js",
+    "revision": "bceea4fa858c798bb6fa201033865b51"
+  },
+  {
+    "url": "build/app/xyfissap.js",
+    "revision": "25d03519f99399851b14fdc57d211da6"
+  },
+  {
+    "url": "build/app/zfdsb0ru.js",
+    "revision": "0bebc52c2d730015319a43b17f521ffc"
   },
   {
     "url": "favicon.ico",
@@ -56,11 +72,15 @@ const fileManifest = [
   },
   {
     "url": "index.html",
-    "revision": "b54a03b3f737c70787067949d2c91027"
+    "revision": "d9234d27d45db5cca57cc84208ead718"
   },
   {
     "url": "todomvc-app-css/index.css",
     "revision": "f29c4945c1d5c1750aaa1b0a47b372e2"
+  },
+  {
+    "url": "workbox-sw.prod.v2.1.1.js",
+    "revision": "2a5638f9e33d09efc487b96804a0aa11"
   }
 ];
 
